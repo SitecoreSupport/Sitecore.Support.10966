73411191a51fff081299ceada79bf6c8a99c684a
DK\IRO <iro@sitecore.net>
2018-04-10 14:29:16 +0300

    Initial

---
+++ src/Sitecore.Support.10966/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.10966")]
+[assembly: AssemblyProduct("Sitecore.Support.10966")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.10966/XA/Foundation/Search/FieldReaders/NameLookupListFieldReader.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.XA.Foundation.Search.FieldReaders
+{
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.FieldReaders;
+  using Sitecore.Data.Fields;
+  using Sitecore.Data.Items;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class NameLookupListFieldReader : FieldReader
+  {
+    public override object GetFieldValue(IIndexableDataField indexableField)
+    {
+      MultilistField multilistField = FieldTypeManager.GetField(indexableField as SitecoreItemDataField) as MultilistField;
+      if (multilistField == null)
+      {
+        return new List<string>();
+      }
+      return (from i in multilistField.GetItems()
+              where i != null
+              select i.Name).ToList();
+    }
+  }
+}
